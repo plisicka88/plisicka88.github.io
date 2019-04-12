dcf2da031122fdea1f3ce2726a5d3702174a2e11
Paulina <paulina.lisicka@gmail.com>
2019-04-11 19:59:44 -0500

    Added project files

---
+++ js/main.js
@@ -0,0 +1,48 @@
+var imagesArray = ['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg','pic5.jpg','pic6.jpg','pic7.jpg','pic8.jpg','pic9.jpg','pic10.jpg']
+
+var imageDescription = [0,0,0,0,0,0,0,0,0,0];
+
+var currentPosition = 0;
+
+var minimum = 0;
+var maximium = imagesArray.length - 1;
+
+$('#yes').on('click', function() {
+	currentPosition += 1;
+
+	
+
+	if (currentPosition >= maximium) {
+		$('#yes').attr('disabled', true);
+	}
+
+	if (currentPosition >= maximium) {
+		$('#no').attr('disabled', true).addClass('noHover');
+	}
+
+	$('#pics').attr('src', 'images/' + imagesArray[currentPosition]);
+
+	if (currentPosition >= maximium) {
+		$('#no').attr('disabled', true).addClass('noHover');
+
+	}
+
+
+	console.log(currentPosition);
+
+});
+
+$('#no').on('click', function () {
+	currentPosition += 1;
+	
+
+	if (currentPosition >= maximium) {
+		$(this).attr('disabled', true);
+	}
+
+	$('#pics').attr('src', 'images/' + imagesArray[currentPosition]);	
+
+	console.log(currentPosition);
+});
+
+$
