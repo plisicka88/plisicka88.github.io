12b930b85c4adbecfe755535a234773934df8eb8
Paulina <paulina.lisicka@gmail.com>
2019-04-15 18:41:22 -0500

    After-Eric

--- js/main.js
+++ js/main.js
@@ -1,91 +1,79 @@
 $(document).ready(function(){
 
-var imagesArray = ['pic1.jpg','pic2.jpg','pic3.jpg','pic4.jpg','pic5.jpg','pic6.jpg','pic7.jpg','pic8.jpg','pic9.jpg','pic10.jpg']
+	var imagesArray = [
+		'pic1.jpg',
+		'pic2.jpg',
+		'pic3.jpg',
+		'pic4.jpg',
+		'pic5.jpg',
+		'pic6.jpg',
+		'pic7.jpg',
+		'pic8.jpg',
+		'pic9.jpg',
+		'pic10.jpg'
+	]
 
-var imageDescription = [0,0,0,0,0,0,0,0,0,0];
+	var imageDescription = [
+		"CATTOS",
+		"Desc 2",
+		"Desc 3",
+		"Desc 4",
+		"Desc 5",
+		"Desc 6",
+		"Desc 7",
+		"Desc 8",
+		"Desc 9",
+		"Desc 10",
+	];
 
-var yesTally = 0;
-var noTally = 0;
+	var yesTally = 0;
 
-var currentPosition = 0;
+	var noTally = 0;
 
-var minimum = 0;
-var maximium = imagesArray.length - 1;
+	var currentPosition = 0;
 
-$('#yes').on('click', function() {
-	currentPosition += 1;
+	var minimum = 0;
+	var maximium = imagesArray.length;
 
-	if (currentPosition >= maximium) {
-		$('#yes').attr('disabled', true);
+	$('#yes').on('click', function() {
+
+		yesTally += 1;
+
+		hey();
+	});
+
+
+	$('#no').on('click', function () {
+
+		noTally += 1;
+
+		hey();
+	});
+
+	function hey(){
+		currentPosition += 1;
+
+		if (currentPosition >= maximium) {
+			// This is what happens at the end
+			$("#yes").hide();
+			$("#no").hide();
+
+			// Show your result
+			if (yesTally == 10){
+				$.featherlight("<div>" + $("#ten").html() + "</div>");
+			} else if (yesTally == 9) {
+				$.featherlight("<div>" + $("#nine").html() + "</div>");
+			}
+
+		} else {
+			// This is what happens ANY other time
+			$('#pics').attr('src', 'images/' + imagesArray[currentPosition]);
+			$('#description').html(imageDescription[currentPosition]);
+		}
+
+		console.log(currentPosition);
+		console.log("Yes Tally is: " + yesTally + " No Tally is: " + noTally);
 	}
-
-	if (currentPosition >= maximium) {
-		$('#no').attr('disabled', true);
-	}
-
-	$('#pics').attr('src', 'images/' + imagesArray[currentPosition]);
-
-	if (currentPosition >= maximium) {
-		$('#no').attr('disabled', true);
-	}
-});
-
-
-$('#no').on('click', function () {
-	currentPosition += 1;
-	
-
-	if (currentPosition >= maximium) {
-		$('#no').attr('disabled', true);
-	}
-
-	if (currentPosition >= maximium) {
-		$('#yes').attr('disabled', true);
-	}
-
-	$('#pics').attr('src', 'images/' + imagesArray[currentPosition]);	
-
-	if (currentPosition >= maximium) {
-		$('#yes').attr('disabled', true);
-	}
-});
-
-
-
-$('#yes').on('click', function (){
-	imageDescription[currentPosition] += 1;
-
-	$('#description').html('Doggos - in various breeds');
-
- // console.log('my image currently is:' + imageDescription);
-
-});
-
-
-$('#no').on('click', function (){
-	imageDescription[currentPosition] += 1;
-
-	$('#description').html("Every Netflix owner's favorite show to binge on");
- // console.log('my image currently is:' + imageDescription);
-});
-
-$('#yes').click(function() {
-	if (currentPosition == 9) {
-		$('#ten').show(1000);	
-	}
-
-	else if (currentPosition >= 8) {
-		$('#nine').show(1000);	
-	}
-
-	console.log('is this working');
-});
-
-// $('#yes').click(function() {
-// 	if (yesTally >= 8) {
-// 		$('#nine').show('fast');	
-// 	}
-
 });
 
 
